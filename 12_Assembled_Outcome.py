6e21dcc4cbdbaff24d2780a56c265573734bebd5
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-02-23 14:32:39 +1300

    complete program

--- 12_Assembled_Outcome.py
+++ 12_Assembled_Outcome.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from functools import partial # to prevent unwanted windows
-import random
 
+import random
 
 class Converter:
     def __init__(self):
@@ -117,7 +117,11 @@ class Converter:
             else:
                 self.converted_label.configure(text=answer, fg="red")
                 self.to_convert_entry.configure(bg=error)
+
             # Add answer to list for History
+            if has_errors != "yes":
+                self.all_calculation.append(answer)
+                self.history_button.config(state=NORMAL)
 
         except ValueError:
             self.converted_label.configure(text="Enter a number!", fg="red")
@@ -130,6 +134,88 @@ class Converter:
             rounded = round(to_round, 1)
 
         return rounded
+    def history(self, calc_history):
+        History(self, calc_history)
+
+class History:
+    def __init__(self, partner, calc_history):
+
+        background = "#E0FFC9" # pale green
+
+        # disable history button
+        partner.history_button.config(state=DISABLED)
+
+            # Sets up child window (ie: history box)
+        self.history_box = Toplevel()
+
+        # If users press cross at top, closes history and 'releases' history button
+        self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+
+        # Set up GUI Frame
+        self.history_frame = Frame(self.history_box, bg=background)
+        self.history_frame.grid()
+
+        # Set up History heading (row 0)
+        self.how_heading = Label(self.history_frame, text="Calculation History",
+                                 font="arial 10 bold", bg=background)
+        self.how_heading.grid(row=0)
+
+        # History text (label, row 1)
+        self.history_text = Label(self.history_frame,
+                                  text="Here are your most recent "
+                                  "calculations. Please use the "
+                                  "export button to create a text "
+                                  "file of all your calculations for "
+                                  "this session", wrap=250,
+                                font="Arial 10 italic",
+                                justify=LEFT, width=40, bg=background,
+                                padx=10, pady=10)
+        self.history_text.grid(column=0, row=1)
+
+        # History output goes here... (row 2)
+
+        # Generaet string from list of calculations...
+        history_string = ""
+
+        if len(calc_history) >= 7:
+            for item in range(0,7):
+                history_string += calc_history[len(calc_history) - item - 1] + "\n"
+
+        else:
+            for item in calc_history:
+                history_string += calc_history[len(calc_history) -
+                                                calc_history.index(item) - 1] + "\n"
+                self.history_text.config(text="Here is your calculation "
+                                              "history. You can use the "
+                                              "export button to save this "
+                                              "data to a text file if "
+                                              "desired.")
+
+        # Label to display calculation history to user
+        self.calc_label = Label(self.history_frame, text=history_string,
+                                bg=background, font="Arial 12", justify=LEFT)
+        self.calc_label.grid(row=2)
+
+        # Export / Dismiss Button Frame (row 3)
+        self.export_dismiss_frame = Frame(self.history_frame)
+        self.export_dismiss_frame.grid(row=3, pady=10)
+
+        # Export button
+        self.export_button = Button(self.export_dismiss_frame, text="Export",
+                                    font="Arial 12 bold")
+        self.export_button.grid(row=0, column=0)
+
+        # Dismss button
+        self.export_button = Button(self.export_dismiss_frame, text="Dismiss",
+                                    font="Arial 12 bold", command=partial(self.close_history, partner))
+        self.export_button.grid(row=0, column=1)
+
+    def close_history(self, partner):
+        # Put history button back to normal...
+        partner.history_button.config(state=NORMAL)
+        self.history_box.destroy()
+
+
 
 # main routine
 if __name__ == "__main__":
