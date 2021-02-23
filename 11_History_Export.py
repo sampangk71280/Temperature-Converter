6f17cd8ad08ca5dbfd53ebc4ca16d25b1c532577
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-02-24 12:59:47 +1300

    exporting part 2

--- 11_History_Export.py
+++ 11_History_Export.py
@@ -36,8 +36,8 @@ class Converter:
                                   padx=10, pady=10, command=lambda: self.history(self.all_calculations))
         self.history_button.grid(row=1)
 
-        if len(self.all_calculation) == 0:
-            self.history_button.config(state=DISABLED)
+        #if len(self.all_calculation) == 0:
+            #self.history_button.config(state=DISABLED)
 
 
     def history(self, calc_history):
@@ -47,6 +47,8 @@ class History:
     def __init__(self, partner, calc_history):
 
         background = "#E0FFC9" # pale green
+
+        calc_history = []
 
         # disable history button
         partner.history_button.config(state=DISABLED)
@@ -108,7 +110,8 @@ class History:
 
         # Export button
         self.export_button = Button(self.export_dismiss_frame, text="Export",
-                                    font="Arial 12 bold")
+                                    font="Arial 12 bold",
+                                    command=lambda: self.export(calc_history))
         self.export_button.grid(row=0, column=0)
 
         # Dismss button
@@ -120,6 +123,49 @@ class History:
         # Put history button back to normal...
         partner.history_button.config(state=NORMAL)
         self.history_box.destroy()
+
+    def export(self, calc_history):
+        Export(self, calc_history)
+
+class Export:
+    def __init__(self, partner, calc_history):
+
+        print(calc_history)
+        background = "#FFD47E"  # pale orange
+
+        # disable export button
+        partner.export_button.config(state=DISABLED)
+
+        # Sets up child window (ie: export box)
+        self.export_box = Toplevel()
+
+        # If users press cross at top, closes export and 'releases' export button
+        self.export_box.protocol('WM_DELETE_WINDOW', partial(self.close_export, partner))
+
+        # Set up GUI Frame
+        self.export_frame = Frame(self.export_box, bg=background)
+        self.export_frame.grid()
+
+        # Set up Export heading (row 0)
+        self.how_heading = Label(self.export_frame, text="Export / Instructions",
+                                 font="arial 10 bold", bg=background)
+        self.how_heading.grid(row=0)
+
+        # Export text (label, row 1)
+        self.export_text = Label(self.export_frame, text="",
+                                 justify=LEFT, width=40, bg=background, wrap=250)
+        self.export_text.grid(column=0, row=1)
+
+        # Dismiss button (row 2)
+        self.dismiss_btn = Button(self.export_frame, text="Dismiss", width=10, bg="orange",
+                                  font="arial 10 bold",
+                                  command=partial(self.close_export, partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_export(self, partner):
+        # Put export button back to normal...
+        partner.export_button.config(state=NORMAL)
+        self.export_box.destroy()
 
 
 
